ec45080da70f89216bee30055c185d9ede55e888
lesiokp <lesiohere@wp.pl>
2025-10-16 20:45:37 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-fetch('https://ourspace-production.up.railway.app/')
+fetch('https://ourspace-production.up.railway.app/api/status')
   .then(response => response.json())
   .then(data => {
     console.log('Backend status:', data);
